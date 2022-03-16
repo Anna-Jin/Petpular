ea4d471d47d534c6215b1d1bfd5c7968b0fe3772
yujin-Jin <appletree066@gmail.com>
2022-03-16 18:39:47 +0900

    이미지 업로드 설정파일 추가

---
+++ src/main/java/com/petpular/common/FileManagerService.java
@@ -0,0 +1,52 @@
+package com.petpular.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileManagerService {
+	
+	public final static String FILE_UPLOAD_PATH = "/Users/jin-yujin/Desktop/yujin/Petpular/workspace/images/";
+	
+	public String savaFile(String userLoginId, MultipartFile file) {
+		String directoryName = userLoginId + "_" + System.currentTimeMillis() + "/";
+		String filePath = FILE_UPLOAD_PATH + directoryName;
+		
+		// 디렉토리 생성
+		File directory = new File(filePath);
+		if (directory.mkdir() == false) {
+			return null; // 디렉토리 생성 시 실패하면 null을 리턴
+		}
+		
+		// 파일 업로드: byte 단위로 업로드한다.
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(filePath + file.getOriginalFilename()); // getOriginalFilename()는 input에 올린 파일명이다. (한글이면 안됨)
+			Files.write(path, bytes);
+			
+			return "/images/" + directoryName + file.getOriginalFilename();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void deleteFile(String imagePath) {
+		Path path = Paths.get(FILE_UPLOAD_PATH + imagePath.replace("/images/", ""));
+		if (Files.exists(path)) {
+			// 이미지 파일이 있으면 삭제
+			try {
+				Files.delete(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
