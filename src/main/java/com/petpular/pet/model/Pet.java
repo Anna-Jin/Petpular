816a672f6663a93fcc399fce7c5441ded2c2a563
Anna-Jin <anna.jin.6868@gmail.com>
2022-03-22 17:04:25 +0900

    클라이언트 <-> 서버 <-> DB snake_case to camelCase 변환 설정 추가

--- src/main/java/com/petpular/pet/model/Pet.java
+++ src/main/java/com/petpular/pet/model/Pet.java
@@ -3,12 +3,16 @@ package com.petpular.pet.model;
 import java.time.LocalDate;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class Pet {
 	private int id;
 	private int userId;
+	
+	@JsonProperty("pet_image_url")
 	private String petImageUrl;
 	private String name;
 	private String species;
