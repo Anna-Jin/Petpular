57f8b30fa36e40da7e675e1df7df41c37d973845
Anna-Jin <anna.jin.6868@gmail.com>
2022-03-22 17:03:37 +0900

    서버 <-> DB간 snake_case to camelCase 변환 설정 추가

--- src/main/java/com/petpular/config/DatabaseConfig.java
+++ src/main/java/com/petpular/config/DatabaseConfig.java
@@ -7,20 +7,19 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 @MapperScan(basePackages="com.petpular.*")
 public class DatabaseConfig {
 
+	
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
-
-        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-        sessionFactory.setMapperLocations(res);
+        sessionFactory.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml"));
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml"));
 
         return sessionFactory.getObject();
     }
