87dc6df445a3aa185ad2109331c829b7e5ddfbfb
yujin-Jin <appletree066@gmail.com>
2022-03-24 16:32:25 +0900

    #20

--- src/main/java/com/petpular/user/UserController.java
+++ src/main/java/com/petpular/user/UserController.java
@@ -1,5 +1,8 @@
 package com.petpular.user;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,9 +18,23 @@ public class UserController {
 		return "user/sign_up";
 	}
 	
+	/**
+	 * 로그인 화면
+	 * @return
+	 */
 	@RequestMapping("/user/login")
 	public String login() {
 		return "user/login";
+	}
+	
+	@RequestMapping("/user/logout")
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		session.removeAttribute("userName");
+		
+		return "redirect:/user/login";
 	}
 	
 }
