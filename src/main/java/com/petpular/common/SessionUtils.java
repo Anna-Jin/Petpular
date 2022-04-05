d2a274f084b6dea03a2c4f33d47c17c6b42bda00
yujin-Jin <appletree066@gmail.com>
2022-04-05 15:19:35 +0900

    #42 MainController와 PetRestController에 있는 session에 담는 코드 util화 시키기

---
+++ src/main/java/com/petpular/common/SessionUtils.java
@@ -0,0 +1,11 @@
+package com.petpular.common;
+
+import java.util.List;
+
+import com.petpular.pet.model.Pet;
+
+public class SessionUtils {
+	public static String petArr(int userId, List<Pet> petList) {
+		return "";
+	}
+}
