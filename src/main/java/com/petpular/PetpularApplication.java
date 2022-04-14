906ad40dbfa5b8c9273ff3ade87ec4eb9de10b3f
yujin-Jin <appletree066@gmail.com>
2022-04-14 17:40:00 +0900

    배포 에러 잡는 중!

--- src/main/java/com/petpular/PetpularApplication.java
+++ src/main/java/com/petpular/PetpularApplication.java
@@ -2,6 +2,7 @@ package com.petpular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,5 +10,10 @@ public class PetpularApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetpularApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PetpularApplication.class);
 	}
 }
