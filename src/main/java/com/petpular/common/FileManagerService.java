68dd2717f5fdfea4661cc7a9ec0d962ee34b65f7
yujin-Jin <appletree066@gmail.com>
2022-04-15 17:31:07 +0900

    에러

--- src/main/java/com/petpular/common/FileManagerService.java
+++ src/main/java/com/petpular/common/FileManagerService.java
@@ -86,6 +86,7 @@ public class FileManagerService {
 		Path path = Paths.get(FILE_UPLOAD_PATH + imagePath.replace("/upload_images/", "").split("/")[0]);
 		if (Files.exists(path)) {
 			// 이미지 파일이 있으면 삭제
+			
 			try {
 				Files.delete(path);
 			} catch (IOException e) {
